75dd53f3b053b2dbd0ef7488707905c14f444b7c
Pedro Ribeiro <ribeiro.tpedro@gmail.com>
2023-11-07 06:44:42 -0300

    create resources for POST, PUT and DELETE routes

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -6,6 +6,7 @@ import { Construct } from 'constructs';
 
 interface ECommerceApiStackProps extends cdk.StackProps {
   productsFetchHandler: lambdaNodeJS.NodejsFunction
+  productsAdminHandler: lambdaNodeJS.NodejsFunction
 }
 
 export class ECommerceApiStack extends cdk.Stack {
@@ -34,10 +35,28 @@ export class ECommerceApiStack extends cdk.Stack {
       }
     })
 
+    // INTEGRAÇÃO FETCH
     const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
 
-    // "/products"
+    // GET "/products"
     const productsResource = api.root.addResource("products")
     productsResource.addMethod("GET", productsFetchIntegration)
+
+    // GET "/products/{id}"
+    const productsIdResource = productsResource.addResource("{id}")
+    productsIdResource.addMethod("GET", productsFetchIntegration)
+
+    // INTEGRAÇÃO ADMIN
+    const productsAdminIntegration = new apigateway.LambdaIntegration(props.productsAdminHandler)
+
+    // POST "/products"
+    productsResource.addMethod("POST", productsAdminIntegration)
+
+    // PUT "/products/{id}"
+    productsIdResource.addMethod("PUT", productsAdminIntegration)
+
+    // DELETE "/products/{id}"
+    productsIdResource.addMethod("DELETE", productsAdminIntegration)
+
   }
 }
