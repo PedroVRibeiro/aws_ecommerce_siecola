b502d7591f516e2a37a818ba2015bb004d67cc8c
Pedro Ribeiro <ribeiro.tpedro@gmail.com>
2023-11-09 07:04:19 -0300

    integrate lambda functions to use layers

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -3,6 +3,7 @@ import * as lambdaNodeJS from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as dynamodb  from "aws-cdk-lib/aws-dynamodb"
+import * as ssm from "aws-cdk-lib/aws-ssm"
 
 export class ProductsAppStack extends cdk.Stack {
   readonly productsFecthHandler: lambdaNodeJS.NodejsFunction;
@@ -24,6 +25,10 @@ export class ProductsAppStack extends cdk.Stack {
       writeCapacity: 1
     })
 
+    // Products Layer (recebimento via parâmetros)
+    const productsLayerArn = ssm.StringParameter.valueForStringParameter(this, "ProductsLayerVersionArn")
+    const productsLayer = lambda.LayerVersion.fromLayerVersionArn(this, "ProductsLayerVersionArn", productsLayerArn)
+
     this.productsFecthHandler = new lambdaNodeJS.NodejsFunction(
       this,
       'ProductsFetchFunction',
@@ -40,7 +45,8 @@ export class ProductsAppStack extends cdk.Stack {
         },
         environment: {
           PRODUCTS_DDB: this.productsDdb.tableName
-        }
+        },
+        layers: [productsLayer]
       });
 
     this.productsDdb.grantReadData(this.productsFecthHandler)
@@ -61,7 +67,8 @@ export class ProductsAppStack extends cdk.Stack {
       },
       environment: {
         PRODUCTS_DDB: this.productsDdb.tableName
-      }
+      },
+      layers: [productsLayer]
     });
 
   this.productsDdb.grantWriteData(this.productsAdminHandler)
